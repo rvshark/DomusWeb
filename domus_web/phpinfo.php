4c227526af8baeb5d15996779b41801f9bde4bc4
luizloja <luizloja@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-01-19 15:28:28 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@233 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/phpinfo.php
+++ domus_web/phpinfo.php
@@ -1,4 +1,24 @@
-Teste Guilherme
+
+<?php
+global $CFG;
+    require_once 'lib/htmlpurifier/library/HTMLPurifier.auto.php';
+      $config = HTMLPurifier_Config::createDefault();
+	//$config->set('HTML', 'DefinitionID', 'enduser-customize1.html tutorial1');
+	//$config->set('HTML', 'DefinitionRev', 2);
+	//$config->set('Core', 'LexerImpl', 'DirectLex');
+        
+        
+        
+        
+	$def2 = $config->getHTMLDefinition(true);
+        
+	
+        $def2->addAttribute('a','id', 'CDATA');
+        $def2->addAttribute('a','name', 'CDATA');
+	$purifier = new HTMLPurifier($config);
+        $teste = $purifier->purify( '<a name="12312" id="dsfasd2" >dsadfasdf</a>' );
+        echo  $teste;
+?>
 
 <?php
 
