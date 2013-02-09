0970951fa16f54ab6618f0ed345b7a2104cfac06
jhonatan <jhonatan@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-02-09 16:10:07 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@254 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/NovoMenuDomus/index.php
+++ domus_web/NovoMenuDomus/index.php
@@ -85,7 +85,7 @@ if ((int)$CFG->version < 2007092000
 if (!isloggedin()) {
 	//require_login();
 	echo "<script>alert('Desculpe, apenas administradores tem acesso a esta pagina')</script>";
-	echo "<script>location.href=(`{$CFG->wwwroot}')</script>";
+	echo "<script>location.href=('http://domus.pucpr.br/homo/')</script>";
 } else {
 	user_accesstime_log();
 	
