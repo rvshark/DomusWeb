78deea74da14e5abc6739416fd7f6e920e93a485
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-04 17:45:38 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@290 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/theme/powerdomus/menu.php
+++ domus_web/theme/powerdomus/menu.php
@@ -289,7 +289,7 @@ function ShowVideo(id_video, titulo)
 		
 		<li><a href="#">Link 4 (com subitens)</a>
 
-				<ul class="ddsmoothmenu-v">
+				<ul id="menuacima" class="ddsmoothmenu-v">
 
 					<li><a href="#">SubLink 1</a></li>
 
