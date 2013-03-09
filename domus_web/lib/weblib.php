661cd0296bb5413bed5858abca5231d6dd2fa9b5
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-09 01:47:46 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@529 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/lib/weblib.php
+++ domus_web/lib/weblib.php
@@ -2472,18 +2472,9 @@ function get_html_lang($dir = false) {
  * @return string HTML element.
  */
 function skip_main_destination() {
-	$link_out = "<a href=javascript:void(0) onclick=ShowVideo('lpE7K768D3A','Trailer08032013');>Trailer de vídeo-aula</a>";
-	$html_out.='<ul id="navAbaixo">';
-	$html_out.="<li>$link_out</li>";
-	$html_out.='</ul>';
-	$server="http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-	$nome_principal=$CFG->wwwroot."/";
-	if($server == $nome_principal){
-    $ss_out=$html_out;
-	}
-  
+
 	
-		 return '<span id="maincontent">'.$ss_out.'</span>';
+		 return '<span id="maincontent"><div><ul id="navAbaixo"><li>sss</li></ul></div></span>;
 
    
 }
