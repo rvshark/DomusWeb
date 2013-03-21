8da56484add2c24edd8ac2693fb5890e2527c1af
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-21 06:43:30 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@622 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/theme/powerdomus/menu.php
+++ domus_web/theme/powerdomus/menu.php
@@ -3,25 +3,48 @@
 
 	<script type="text/javascript">
 
-	function TestaCPF(strCPF) {
-	    var Soma;
-	    var Resto;
-	    Soma = 0;
-	    if (strCPF == "00000000000") return false;
+	function TestaCPF(cpf) {
 
-	    for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-	    Resto = (Soma * 10) % 11;
+	    cpf = cpf.replace(/[^\d]+/g,'');
 
-	    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-	    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+	    if(cpf == '') return false;
 
-	    Soma = 0;
-	    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-	    Resto = (Soma * 10) % 11;
+	    // Elimina CPFs invalidos conhecidos
+	    if (cpf.length != 11 || 
+	        cpf == "00000000000" || 
+	        cpf == "11111111111" || 
+	        cpf == "22222222222" || 
+	        cpf == "33333333333" || 
+	        cpf == "44444444444" || 
+	        cpf == "55555555555" || 
+	        cpf == "66666666666" || 
+	        cpf == "77777777777" || 
+	        cpf == "88888888888" || 
+	        cpf == "99999999999")
+	        return false;
 
-	    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-	    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+	    // Valida 1o digito
+	    add = 0;
+	    for (i=0; i < 9; i ++)
+	        add += parseInt(cpf.charAt(i)) * (10 - i);
+	    rev = 11 - (add % 11);
+	    if (rev == 10 || rev == 11)
+	        rev = 0;
+	    if (rev != parseInt(cpf.charAt(9)))
+	        return false;
+
+	    // Valida 2o digito
+	    add = 0;
+	    for (i = 0; i < 10; i ++)
+	        add += parseInt(cpf.charAt(i)) * (11 - i);
+	    rev = 11 - (add % 11);
+	    if (rev == 10 || rev == 11)
+	        rev = 0;
+	    if (rev != parseInt(cpf.charAt(10)))
+	        return false;
+
 	    return true;
+
 	}
 
 
