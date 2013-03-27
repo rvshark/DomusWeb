86480b3d294462f0cc59222bf81f24b1b0ba036c
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-27 08:07:30 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@709 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/theme/powerdomus/menu.php
+++ domus_web/theme/powerdomus/menu.php
@@ -87,9 +87,7 @@
 
 					for (var i = 0; i < j.length; i++) {
 						if(j[i].id_cidade == cidade){
-					options += '<option value="'+j[i].id_cidade+'" selected="selected">' + j[i].nome + '</option>';
-						}else{
-							options += '<option value="' + j[i].id_cidade + '">' + j[i].nome + '</option>';
+				    	options += '<option value="'+j[i].id_cidade+'" selected="selected">' + j[i].nome + '</option>';
 						}
 
 					}
