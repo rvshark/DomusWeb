8da1e70236e44479266274d781b6efa7af7e286f
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-27 07:18:06 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@688 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/theme/powerdomus/menu.php
+++ domus_web/theme/powerdomus/menu.php
@@ -130,6 +130,50 @@
 					
 					
 			}
+			}else{
+					$.getJSON('../../lib/cidades.php?search=',{id_estado: estado, ajax: 'true'}, function(j){
+						var options = '<select name="cidades_go"  id="cidades_go">';
+					    options+='<option value="">-- Escolha uma cidade --</option>';
+						options+='<option value=""></option>';	
+
+						for (var i = 0; i < j.length; i++) {
+							if(j[i].id_cidade == cidade){
+						options += '<option value="'+j[i].id_cidade+'" selected="selected">' + j[i].nome + '</option>';
+							}else{
+						options += '<option value="' + j[i].id_cidade + '">' + j[i].nome + '</option>';
+							}
+
+						}
+					    options+="</select>";
+
+						$('#carregando_cidade').hide();
+						$("#cidade_div_append").append(options);
+
+
+						$('#cidades_go').show();
+
+
+					});
+
+						$.getJSON('../../lib/estados.php?search=',{ ajax: 'true'}, function(j){
+							 var estados_options = '<select name="estados_go" id="estados_go">';
+						    estados_options+='<option value="">-- Escolha um estado --</option>';
+
+
+							for (var i = 0; i < j.length; i++) {
+								if(j[i].id_estado == estado){
+						estados_options += '<option value="'+j[i].id_estado+'" selected="selected">' + j[i].nome_estado + '</option>';
+								}else{
+							estados_options += '<option value="' + j[i].id_estado + '">' + j[i].nome_estado + '</option>';
+								}
+
+							}
+						    estados_options+="</select>";
+
+							$("#div_estado_append").append(estados_options);
+
+
+						});	
 			}
 
 		}
