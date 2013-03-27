9fe7942ffe33c4e780e7cfec12e88c54f7ce7109
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-27 06:21:40 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@666 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/theme/powerdomus/menu.php
+++ domus_web/theme/powerdomus/menu.php
@@ -315,7 +315,7 @@
 	
 	//Iniciliza os campos do formulário
 	function inicializaCampos(){
-	    //$('#trNome,#trTelefone,#trCPF,#trCidade,#trPais,#trInstituicao,#trEstado').hide();
+	    $('#trNome,#trTelefone,#trCPF,#trCidade,#trPais,#trInstituicao,#trEstado').hide();
 		$("#txtNome,#txtTelefone,#txtInstituicao").val('');
 		$('#txtPais').val(0);  			
 	}
