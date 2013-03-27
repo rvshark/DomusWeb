317091fadeafba788b0837290acced7b20e6500f
guizao <guizao@c97a1ace-34ef-4a5b-a364-613dd0cf9b8b>
2013-03-27 06:00:32 +0000

    git-svn-id: https://mikeoxlong/svn/teste/trunk@658 c97a1ace-34ef-4a5b-a364-613dd0cf9b8b

--- domus_web/theme/powerdomus/menu.php
+++ domus_web/theme/powerdomus/menu.php
@@ -68,36 +68,36 @@
 
 					}
 				    options+="</select>";
-				     var estados_options = '<select name="estados_go" id="estados_go">
-					<option value="">-- Selecione o estado --</option>
-					<option value="1">Acre</option>
-					<option value="2">Alagoas</option>
-					<option value="4">Amapá</option>
-					<option value="3">Amazonas</option>
-					<option value="5">Bahia</option>
-					<option value="6">Ceará</option>
-					<option value="7">Destrito Federal</option>
-					<option value="8">Espírito Santo</option>
-					<option value="9">Goiás</option>
-					<option value="10">Maranhão</option>
-					<option value="13">Mato Grosso</option>
-					<option value="12">Mato Grosso do Sul</option>
-					<option value="11">Minas Gerais</option>
-					<option value="14">Pará</option>
-					<option value="15">Paraíba</option>
-					<option value="18">Paraná</option>
-					<option value="16">Pernambuco</option>
-					<option value="17">Piauí</option>
-					<option value="19">Rio de Janeiro</option>
-					<option value="20">Rio Grande do Norte</option>
-					<option value="23">Rio Grandre do Sul</option>
-					<option value="21">Rondônia</option>
-					<option value="22">Rorâima</option>
-					<option value="24">Santa Catarina</option>
-					<option value="26">São Paulo</option>
-					<option value="25">Sergipe</option>
-					<option value="27">Tocantins</option>
-					</select>';
+				     var estados_options = '<select name="estados_go" id="estados_go">';
+				    estados_options+='<option value="">-- Selecione o estado --</option>';
+					estados_options+='<option value="1">Acre</option>';
+					estados_options+='<option value="2">Alagoas</option>';
+					estados_options+='<option value="4">Amapá</option>';
+					estados_options+='<option value="3">Amazonas</option>';
+					estados_options+='<option value="5">Bahia</option>v
+					estados_options+='<option value="6">Ceará</option>';
+					estados_options+='<option value="7">Destrito Federal</option>';
+					estados_options+='<option value="8">Espírito Santo</option>';
+					estados_options+='<option value="9">Goiás</option>';
+					estados_options+='<option value="10">Maranhão</option>';
+					estados_options+='<option value="13">Mato Grosso</option>';
+					estados_options+='<option value="12">Mato Grosso do Sul</option>';
+					estados_options+='<option value="11">Minas Gerais</option>';
+					estados_options+='<option value="14">Pará</option>';
+					estados_options+='<option value="15">Paraíba</option>';
+					estados_options+='<option value="18">Paraná</option>';
+					estados_options+='v<option value="16">Pernambuco</option>';
+					estados_options+='<option value="17">Piauí</option>';
+					estados_options+='<option value="19">Rio de Janeiro</option>';
+					estados_options+='<option value="20">Rio Grande do Norte</option>';
+					estados_options+='<option value="23">Rio Grandre do Sul</option>';
+					estados_options+='<option value="21">Rondônia</option>';
+					estados_options+='<option value="22">Rorâima</option>';
+					estados_options+='<option value="24">Santa Catarina</option>';
+					estados_options+='<option value="26">São Paulo</option>';
+					estados_options+='<option value="25">Sergipe</option>';
+					estados_options+='<option value="27">Tocantins</option>';
+					estados_options+='</select>';
 					$('#carregando_cidade').hide();
 					$("#cidade_div_append").append(options);
 					
